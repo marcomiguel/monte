a67c1eff7b388ee75c0227d3b136bc512c2f47ee
Marco Montenegro <dev-web07@rppnoticias.pe>
2015-12-15 19:05:31 -0500

    crud basico v0.1

--- express/montex/routes/crud.js
+++ express/montex/routes/crud.js
@@ -1,9 +1,54 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('crud', { title: 'Crud' });
+// Conexion BD
+mongoose.connect('mongodb://localhost:27017/montex');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  
+});
+
+var todoSchema = mongoose.Schema({
+  text: String
+});
+var Todo = mongoose.model('todos', todoSchema);
+
+// Listar
+router.get('/todos',function(req, res){
+  Todo.find(function(err, todos){
+    if(err) res.send(err);
+     res.json(todos);
+  });
+});
+
+// Agregar
+router.post('/todos', function(req, res){
+  Todo.create({
+    text:req.body.text,
+    done:false
+  },function(err, todo){
+    if(err) res.send(err);
+    Todo.find(function(err,todos){
+      if(err) res.send(err);
+      res.json(todos);  
+    });
+  });
+});
+
+// Eliminar
+router.delete('/todos/:todo',function(req, res){
+  Todo.remove({
+    _id:req.params.todo
+  },function(err, todo){
+    if(err) res.send(err);
+    Todo.find(function(err, todos){
+      if(err) res.send(err);
+      res.json(todos);
+    });
+  });
 });
 
 module.exports = router;
